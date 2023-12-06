3b8fd34c399bc3c10fdbb0d772511edf0ef4fb32
yesiamok <jaehyunok@cau.ac.kr>
2023-12-06 22:47:43 +0900

    trilateration

--- app/src/main/java/com/iot8/localization8/MainActivity.java
+++ app/src/main/java/com/iot8/localization8/MainActivity.java
@@ -17,12 +17,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import org.apache.commons.math3.analysis.solvers.*;
-
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     TextView ap1;
     TextView ap2;
     TextView ap3;
+    TextView currentPosition;
 
     WifiManager wifiManager;
     List<ScanResult> scanResultList;
@@ -43,16 +40,24 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
-            if (action.equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
-                getWifiInfo();
+            if (action.equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+                try {
+                    getWifiInfo();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
     };
 
+    // TODO: 비콘의 위치와 거리를 설정
+    double[][] points = {{1.0, 1.0}, {10.0, 1.0}, {5.0, 10.0}};
+    double[] distance = {0.0, 0.0, 0.0};
 
-    private void getWifiInfo() {
+
+    private void getWifiInfo() throws InterruptedException {
         if(!doneWifiScan) { // wifiScan을 한 경우에만 getScanResult를 사용하도록 flag 변수 구현
             scanResultList = wifiManager.getScanResults();
-            Set<ScanResult> scanResultSet = new HashSet<>(scanResultList);
 
             String str;
             //중복되지 않게 해당 num번째 배열에 AP정보들을 집어넣는다.
@@ -61,19 +66,37 @@ public class MainActivity extends AppCompatActivity {
                 // 화면의 TextView에 SSID와 BSSID를 이어붙여서 텍스트로 표시
                 str = result.SSID;
                 System.out.println(str + result.level);
-                double level = calculateDistance(result.level, -50, 2.0);
-                if (result.SSID.equals("swcse601")) {
-                    ap1.setText(String.valueOf(level));
+                // TODO: 1m 기준일 때 신호세기, 얼마나 방해를 많이 받는지 설정
+                double level = calculateDistance(result.level, -50, 3.5);
+                if (result.SSID.equals("U+Net0DB8")) {
+                    ap1.setText(String.valueOf(level) + ' ' + result.level);
+                    distance[0] = level;
                 }
-                if (result.SSID.equals("Smart-CAU_2.4G")) {
-                    ap2.setText(String.valueOf(level));
+                if (result.SSID.equals("SK_WiFiGIGA5CB8_2.4G")) {
+                    ap2.setText(String.valueOf(level) + ' ' + result.level);
+                    distance[1] = level;
                 }
-                if (result.SSID.equals("옥재")) {
-                    ap3.setText(String.valueOf(level));
+                if (result.SSID.equals("yesiamok")) {
+                    ap3.setText(String.valueOf(level) + ' ' + result.level);
+                    distance[2] = level;
                 }
             }
 
             doneWifiScan = true;
+
+            if (ap1.getText().toString().isEmpty() || ap2.getText().toString().isEmpty() || ap3.getText().toString().isEmpty()) {
+                doneWifiScan = false;
+                Thread.sleep(3000);
+                scanWiFi();
+            } else {
+                try {
+                    double[] result = trilaterate(points[0], distance[0], points[1], distance[1], points[2], distance[2]);
+                    String resultText = "x: " + result[0] + " y: " + result[1];
+                    currentPosition.setText(resultText);
+                } catch (IllegalArgumentException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
         }
     }
 
@@ -81,6 +104,86 @@ public class MainActivity extends AppCompatActivity {
         return Math.pow(10.0, ((rssi - referenceSignalStrength) / (-10.0 * pathLossExponent)));
     }
 
+    public static double[] trilaterate(double[] point1, double distance1, double[] point2, double distance2, double[] point3, double distance3) {
+        // Check for a degenerate case: all three points are collinear
+        if (areCollinear(point1, point2, point3)) {
+            throw new IllegalArgumentException("Trilateration is not possible. The three points are collinear.");
+        }
+
+        // Use an iterative optimization method to find the unknown point
+        double[] unknownPoint = new double[]{0, 0}; // Initial guess
+
+        for (int i = 0; i < 100; i++) {
+            double[] residuals = computeResiduals(unknownPoint, point1, distance1, point2, distance2, point3, distance3);
+            double[][] jacobian = computeJacobian(unknownPoint, point1, distance1, point2, distance2, point3, distance3);
+
+            try {
+                double[] delta = solveLinearSystem(jacobian, residuals);
+                unknownPoint[0] -= delta[0];
+                unknownPoint[1] -= delta[1];
+
+                // Check for convergence
+                if (Math.abs(delta[0]) < 1e-6 && Math.abs(delta[1]) < 1e-6) {
+                    break;
+                }
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("Singular matrix during optimization. Trilateration may not converge.");
+            }
+        }
+
+        return unknownPoint;
+    }
+
+    private static boolean areCollinear(double[] point1, double[] point2, double[] point3) {
+        // Check if the three points are collinear
+        double area = 0.5 * (point1[0] * (point2[1] - point3[1]) + point2[0] * (point3[1] - point1[1]) + point3[0] * (point1[1] - point2[1]));
+        return Math.abs(area) < 1e-10;
+    }
+
+    private static double[][] computeJacobian(double[] point, double[] point1, double distance1, double[] point2, double distance2, double[] point3, double distance3) {
+        double[][] jacobian = new double[3][2];
+
+        // Numerical differentiation to compute the Jacobian
+        double epsilon = 1e-6;
+
+        for (int i = 0; i < 2; i++) {
+            double[] perturbedPoint = point.clone();
+            perturbedPoint[i] += epsilon;
+
+            double[] perturbedResiduals = computeResiduals(perturbedPoint, point1, distance1, point2, distance2, point3, distance3);
+
+            for (int j = 0; j < 3; j++) {
+                jacobian[j][i] = (perturbedResiduals[j] - computeResiduals(point, point1, distance1, point2, distance2, point3, distance3)[j]) / epsilon;
+            }
+        }
+
+        return jacobian;
+    }
+
+    private static double[] solveLinearSystem(double[][] A, double[] b) {
+        // Solve the linear system using Gauss elimination or another method
+        double det = A[0][0] * A[1][1] - A[0][1] * A[1][0];
+
+        if (Math.abs(det) < 1e-10) {
+            throw new IllegalArgumentException("Singular matrix, cannot solve the system");
+        }
+
+        double[] solution = new double[2];
+        solution[0] = (A[1][1] * b[0] - A[0][1] * b[1]) / det;
+        solution[1] = (-A[1][0] * b[0] + A[0][0] * b[1]) / det;
+
+        return solution;
+    }
+
+    private static double[] computeResiduals(double[] point, double[] point1, double distance1, double[] point2, double distance2, double[] point3, double distance3) {
+        double[] residuals = new double[3];
+
+        residuals[0] = Math.sqrt(Math.pow(point[0] - point1[0], 2) + Math.pow(point[1] - point1[1], 2)) - distance1;
+        residuals[1] = Math.sqrt(Math.pow(point[0] - point2[0], 2) + Math.pow(point[1] - point2[1], 2)) - distance2;
+        residuals[2] = Math.sqrt(Math.pow(point[0] - point3[0], 2) + Math.pow(point[1] - point3[1], 2)) - distance3;
+
+        return residuals;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -92,6 +195,7 @@ public class MainActivity extends AppCompatActivity {
         ap1 = (TextView)findViewById(R.id.ap1);
         ap2 = (TextView)findViewById(R.id.ap2);
         ap3 = (TextView)findViewById(R.id.ap3);
+        currentPosition = (TextView)findViewById(R.id.currentPosition);
 
         arrayList = new ArrayList<>();
         arrayList.add("AP1");
@@ -123,17 +227,21 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view) {
         if(view.getId() == R.id.start) {
             // Start wifi scan 버튼이 눌렸을 때
-            doneWifiScan = false;
-            Toast.makeText(this, "WiFi scan start!!", Toast.LENGTH_LONG).show();
+            scanWiFi();
+        }
+    }
 
-            if(isPermitted) {
-                // wifi 스캔 시작
-                wifiManager.startScan();
-                isWifiScan = true;
-            } else {
-                Toast.makeText(getApplicationContext(),
-                        "Location access 권한이 없습니다..", Toast.LENGTH_LONG).show();
-            }
+    public void scanWiFi() {
+        doneWifiScan = false;
+        Toast.makeText(this, "WiFi scan start!!", Toast.LENGTH_LONG).show();
+
+        if(isPermitted) {
+            // wifi 스캔 시작
+            wifiManager.startScan();
+            isWifiScan = true;
+        } else {
+            Toast.makeText(getApplicationContext(),
+                    "Location access 권한이 없습니다..", Toast.LENGTH_LONG).show();
         }
     }
 
